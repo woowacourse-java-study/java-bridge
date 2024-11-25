8c34a69a9dfdffe865420522d57b679f7a03db2d
이창근 <dlckdrms0517@gmail.com>
2024-11-25 15:57:36 +0900

    feat: CustomExceptions 작성

---
+++ src/main/java/bridge/exception/CustomExceptions.java
@@ -0,0 +1,26 @@
+package bridge.exception;
+
+public enum CustomExceptions {
+	
+	INVALID_MOVE(
+			"유효하지 않은 움직임입니다.",
+			IllegalArgumentException.class
+	),
+	;
+	
+	private final String message;
+	private final Class<? extends RuntimeException> exceptionType;
+	
+	CustomExceptions(String message, Class<? extends RuntimeException> exceptionType) {
+		this.message = message;
+		this.exceptionType = exceptionType;
+	}
+	
+	public RuntimeException get() {
+		try {
+			return exceptionType.getDeclaredConstructor(String.class).newInstance(message);
+		} catch (Exception e) {
+			return new RuntimeException(message);
+		}
+	}
+}
