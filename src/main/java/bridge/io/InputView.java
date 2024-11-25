88681524116b3f198f942c86f924f8209090eb6c
이창근 <dlckdrms0517@gmail.com>
2024-11-25 17:43:18 +0900

    fix: 출력 포매팅

--- src/main/java/bridge/io/InputView.java
+++ src/main/java/bridge/io/InputView.java
@@ -16,17 +16,17 @@ public class InputView {
     }
     
     public int readBridgeSize() {
-        writer.write("다리의 길이를 입력해주세요.\n");
+        writer.write("\n다리의 길이를 입력해주세요.\n");
         return Integer.parseInt(reader.readLine());
     }
 
     public Move readMoving() {
-        writer.write("이동할 칸을 선택해주세요. (위: U, 아래: D)\n");
+        writer.write("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n");
         return Move.from(reader.readLine());
     }
 
     public RestartCommand readGameCommand() {
-        writer.write("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        writer.write("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n");
         return RestartCommand.from(reader.readLine());
     }
 }
--- src/main/java/bridge/io/OutputView.java
+++ src/main/java/bridge/io/OutputView.java
@@ -4,9 +4,6 @@ import bridge.domain.GameResult;
 import bridge.domain.MoveStatus;
 import bridge.io.writer.Writer;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
     
     private final Writer writer;
@@ -16,7 +13,7 @@ public class OutputView {
     }
     
     public void printGreetings() {
-        writer.write("다리 건너기 게임을 시작합니다.\n\n");
+        writer.write("다리 건너기 게임을 시작합니다.\n");
     }
     
     public void printMap(MoveStatus moveStatus) {
@@ -25,13 +22,11 @@ public class OutputView {
 
     public void printResult(GameResult gameResult) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("최종 게임 결과\n");
+        stringBuilder.append("\n최종 게임 결과\n");
         stringBuilder.append(gameResult.getMoveStatus());
-        stringBuilder.append("\n\n");
-        stringBuilder.append("게임 성공 여부: ")
-                .append(getSucessResult(gameResult))
-                .append("\n");
-        stringBuilder.append("총 시도한 횟수: ")
+        stringBuilder.append("\n게임 성공 여부: ")
+                .append(getSucessResult(gameResult));
+        stringBuilder.append("\n총 시도한 횟수: ")
                 .append(gameResult.getTotalTryCount());
         writer.write(stringBuilder.toString());
     }
