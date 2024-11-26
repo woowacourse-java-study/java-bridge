6353601bf9df9a9268ad8331fc2506c35555e566
이창근 <dlckdrms0517@gmail.com>
2024-11-26 22:08:45 +0900

    chore: Application 모듈 의존성 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,19 +10,27 @@ import bridge.io.reader.MissionUtilsReader;
 import bridge.io.reader.Reader;
 import bridge.io.writer.ConsoleWriter;
 import bridge.io.writer.Writer;
+import bridge.service.bridgeService.BridgeServiceRetryHandler;
+import bridge.service.bridgeService.DefaultBridgeService;
+import bridge.service.retryHandler.RetryHandler;
 
 public class Application {
 
     public static void main(String[] args) {
         Reader reader = new MissionUtilsReader();
         Writer writer = new ConsoleWriter();
+        ExceptionHandler exceptionHandler = new ExceptionHandler(writer);
         Controller controller = new BridgeController(
                 new InputView(reader, writer),
-                new OutputView(writer)
+                new OutputView(writer),
+                new BridgeServiceRetryHandler(
+                        new DefaultBridgeService(),
+                        new RetryHandler(exceptionHandler)
+                )
         );
         new ControllerExceptionHandleProxy(
                 controller,
-                new ExceptionHandler(writer)
+                exceptionHandler
         ).run();
     }
 }
