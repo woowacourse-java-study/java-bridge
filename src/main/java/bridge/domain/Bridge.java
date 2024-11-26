5e0f9aa5e86725fb4bf3d5a1e159222a94de68a0
이창근 <dlckdrms0517@gmail.com>
2024-11-26 21:29:06 +0900

    feat: 다리 길이 검증 기능 개발

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,15 +1,28 @@
 package bridge.domain;
 
 import bridge.dto.MoveResult;
+import bridge.exception.CustomExceptions;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Bridge {
+	
+	private static final int MIN_STEP_SIZE = 3;
+	private static final int MAX_STEP_SIZE = 8;
 	
 	private final List<Step> steps;
 	
 	public Bridge(List<Step> steps) {
+		Objects.requireNonNull(steps);
+		validate(steps);
 		this.steps = steps;
+	}
+	
+	private static void validate(List<Step> steps) {
+		if (steps.size() < MIN_STEP_SIZE || steps.size() > MAX_STEP_SIZE) {
+			throw CustomExceptions.INVALID_BRIDGE_SIZE.get();
+		}
 	}
 	
 	public static Bridge from(int size, BridgeMaker bridgeMaker) {
