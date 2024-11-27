2735f6746da1ff97743b53a3325c4461d4705971
이창근 <dlckdrms0517@gmail.com>
2024-11-27 14:17:57 +0900

    test: 통합테스트 작성

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -16,15 +16,7 @@ class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
-    void 다리_생성_테스트() {
-        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        List<String> bridge = bridgeMaker.makeBridge(3);
-        assertThat(bridge).containsExactly("U", "D", "D");
-    }
-
-    @Test
-    void 기능_테스트() {
+    void 한번에_게임을_끝낸다() {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
@@ -40,31 +32,85 @@ class ApplicationTest extends NsTest {
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1);
     }
+    
+    @Test
+    void 여러번_시도하여_게임을_끝낸다() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+            
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+    
+    @Test
+    void 실패하고_게임을_종료한다() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+            
+            int upSideIndex = output().indexOf("[ O | X ]");
+            int downSideIndex = output().indexOf("[   |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
 
     @Test
-    void 예외_테스트() {
+    void 다리_길이를_잘못_입력한다() {
         assertSimpleTest(() -> {
             runException("a");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+    
+    @Test
+    void 다리_길이를_짧게_입력한다() {
+        assertSimpleTest(() -> {
+            runException("2");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    
+    @Test
+    void 다리_길이를_길게_입력한다() {
+        assertSimpleTest(() -> {
+            runException("21");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    
+    @Test
+    void 움직임을_잘못_입력한다() {
+        assertSimpleTest(() -> {
+            runException("3", "a");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    
+    @Test
+    void 재시도를_잘못_입력한다() {
+        assertRandomNumberInRangeTest(() -> {
+            runException("3", "U", "U", "a");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        }, 1, 0, 1);
+    }
 
     @Override
     protected void runMain() {
         Application.main(new String[]{});
-    }
-
-    static class TestNumberGenerator implements BridgeNumberGenerator {
-
-        private final List<Integer> numbers;
-
-        TestNumberGenerator(List<Integer> numbers) {
-            this.numbers = numbers;
-        }
-
-        @Override
-        public int generate() {
-            return numbers.remove(0);
-        }
     }
 }
---
+++ src/test/java/bridge/testUtil/TestNumberGenerator.java
@@ -0,0 +1,19 @@
+package bridge.testUtil;
+
+import bridge.service.bridgeNumberGenerator.BridgeNumberGenerator;
+
+import java.util.List;
+
+public class TestNumberGenerator implements BridgeNumberGenerator {
+	
+	private final List<Integer> numbers;
+	
+	public TestNumberGenerator(List<Integer> numbers) {
+		this.numbers = numbers;
+	}
+	
+	@Override
+	public int generate() {
+		return numbers.removeFirst();
+	}
+}
