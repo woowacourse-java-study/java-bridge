bc8a6fb21186a0f47449757ab147f28d497c45b7
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-29 19:54:50 +0900

    feat(constant): 상수 추출

---
+++ src/main/java/bridge/constant/GameValue.java
@@ -0,0 +1,11 @@
+package bridge.constant;
+
+public class GameValue {
+    private GameValue() {
+    }
+
+    public static final String UP = "U";
+    public static final String DOWN = "D";
+    public static final String RETRY = "R";
+    public static final String QUIT = "Q";
+}
